efc7d70eb6c0391fbb965d25f73226dbaa8b785f
Cody Bontecou <bontecouc@gmail.com>
2021-10-03 10:02:10 -0500

    No-posts state for feed component

--- components/Feed/Feed.vue
+++ components/Feed/Feed.vue
@@ -1,19 +1,26 @@
 <template>
-  <div class="flex flex-col">
-    <div class="flex justify-between items-center">
-      <h1 class="font-bold text-3xl md:text-5xl">Your Posts</h1>
-      <NuxtLink to="/post">
-        <Button />
-      </NuxtLink>
+  <div>
+    <div class="flex flex-col">
+      <div class="flex justify-between items-center">
+        <h1 class="font-bold text-3xl md:text-5xl">Your Posts</h1>
+        <NuxtLink to="/post">
+          <Button />
+        </NuxtLink>
+      </div>
+      <PostRow
+        v-for="(post, i) in posts"
+        :key="i"
+        :post="post"
+        :class="{
+          'mt-10': i === 0,
+        }"
+      />
+      <div v-if="posts.length === 0" class="flex items-center justify-center">
+        <p class="mt-10 font-medium text-gray-400">
+          You haven't posted anything yet
+        </p>
+      </div>
     </div>
-    <PostRow
-      v-for="(post, i) in posts"
-      :key="i"
-      :post="post"
-      :class="{
-        'mt-10': i === 0,
-      }"
-    />
   </div>
 </template>
 
